55354491e960a5a5791682285b9d99b57d387c7b
gaya3naik <gaya3naik@gmail.com>
2015-08-12 17:47:09 +0530

    Changes to run query and get data from SF

---
+++ src/main/java/com/salesforce/heroku/api/AnalyticsRestAPI.java
@@ -0,0 +1,38 @@
+package com.salesforce.heroku.api;
+
+import com.dreamforceDemo.utils.SalesforceQueryRunner;
+import com.google.common.collect.Lists;
+import com.salesforce.heroku.bean.Request;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by gayathri on 11/8/15.
+ */
+
+@Path("trends")
+public class AnalyticsRestAPI {
+
+    @POST
+    @Path("/analysis")
+    public List<Map<String, Object>> fetchData(Request request){
+        List records = Lists.newArrayList();
+                String query = "SELECT Id,PageViews__c,SessionCount__c, Date__c FROM AnalyticsData__c";
+        try {
+            Map<String, Object> sfResult = SalesforceQueryRunner.query(query, request.getUrl(), request.getSessionId());
+            records = (List<Map<String, Object>>) sfResult.get("records");
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return records;
+    }
+
+}
