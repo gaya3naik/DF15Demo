7e87b36e401fcd55e4db2f99d541aadde1298dcb
gaya3naik <gaya3naik@gmail.com>
2015-08-12 18:01:46 +0530

    Missed file from the previous commit

---
+++ src/main/java/com/salesforce/heroku/service/TrendAnalysisServiceImpl.java
@@ -0,0 +1,14 @@
+package com.salesforce.heroku.service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by gayathri on 12/8/15.
+ */
+public class TrendAnalysisServiceImpl {
+
+    public static List<Map<String, Object>> getAnalyzedTrendData(String analysisType){
+        return null;
+    }
+}
